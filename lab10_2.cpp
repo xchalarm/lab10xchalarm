8f005407a49d5c8388fba378b4c68b908544757d
xchalarm <120165210+xchalarm@users.noreply.github.com>
2024-12-20 16:30:38 +0700

    Update lab10_2.cpp

--- lab10_2.cpp
+++ lab10_2.cpp
@@ -6,11 +6,16 @@ using namespace std;
 int main (){
 	ifstream source;
 	ofstream dest;
-	source.open("cheerbook.txt") ;
+	source.open("cheerbook.txt");
 	dest.open("cheerbook_copy.txt");
 	
-	//กรอก Code เฉพาะส่วนนี้ ให้สอดคล้องกับโจทย์และ Code ส่วนอื่น ๆ 
-	//โดยห้ามแก้ไข Code ในส่วนอื่น ๆ
+	dest << "-------------------- BOOM ---------------------" << "\n" ;
+	string x ;
+	while (getline(source,x)){
+	    dest << x << endl;
+	}
+	dest << "-------------------- HA!! ---------------------" << endl ;
+	
 
     source.close();
     dest.close();
